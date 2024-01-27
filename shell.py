7070231c5e2a2203bee688f9a19993a65baa86be
Tornado Azul <40547556+TornadoAzul@users.noreply.github.com>
2024-01-26 23:32:27 -0400

    Añadimos la shell del lenguaje.

---
+++ shell.py
@@ -0,0 +1,14 @@
+# # # # # # # # # # # # # # # #
+# Bonus Programming Languaje  #
+# # # # # # # # # # # # # # # #
+
+# • ~ + Creado por ellos + ~ • #
+# • ~ + Created by them  + ~ • #
+
+# ~ Tornado Azul
+# • Pronto más gente •
+
+# Acá comienza el código
+
+while True:
+    text = input('basic > ')
